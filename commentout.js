539cdba42907463cc7f9db6d5910d16f0cef9303
Ephraim Park <ephiepark@gmail.com>
2016-05-30 17:25:56 +0900

    change background color of comments with an image

--- commentout.js
+++ commentout.js
@@ -1,1 +1,11 @@
-document.write("Test");
+var hideImage = function() {
+    $("body").bind("DOMSubtreeModified", function() {
+        $(".UFIComment").each( function(index) {
+            if ($(this).find(".mvs").length != 0) {
+                $(this).css("background-color", "red");
+            }
+        }); 
+    });
+};
+
+hideImage();
