0ad9412d90e3833eebe0f646a7e64a79d01e8807
Ephraim Park <ephiepark@gmail.com>
2016-06-06 15:14:24 +0900

    removed class

--- commentout.js
+++ commentout.js
@@ -1,11 +1,33 @@
-var hideImage = function() {
+var rules = [
+    function(comment) {
+        if ($(comment).find(".mvs").length != 0) {
+            return true;
+        }
+        return false;
+    }
+];
+
+var checkComment = function(comment) {
+    for (var i=0; i<rules.length; i++) {
+        if (rules[i](comment)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+var hideComment = function() {
     $("body").bind("DOMSubtreeModified", function() {
-        $(".UFIComment").each( function(index) {
-            if ($(this).find(".mvs").length != 0) {
-                $(this).css("background-color", "red");
+        $(".UFIComment").each(function(index) {
+            if (checkComment(this)) {
+                if (!$(this).hasClass("removed")) {
+                    $(this).css("background-color", "red");
+                    $(this).addClass("removed");
+                    $(this).append("<p>Removed</p>");
+                }
             }
         }); 
     });
 };
 
-hideImage();
+hideComment();
