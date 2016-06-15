a2a08f38ae7fa92a4d2920c4085b9f1a1c65c996
Min Woo Kim <MinWooKim@Mins-MacBook-Air.local>
2016-06-15 14:10:20 +0900

    Recognize image & fix button styles

--- commentout.js
+++ commentout.js
@@ -1,67 +1,76 @@
-var rules = [
-    function(comment) {
-        var comment_str = $(comment).text();
-        console.log("image check!");
-        console.log(comment_str.length);
-        console.log($(comment).find("img").length);
-        console.log(comment_str);
-        console.log($(comment));
-        if ($(comment).find(".mvs").length != 0) {
-            return true;
-            console.log("ASFASDFADSFDSAFADSF");
-        }
-        return false;
-    },
-    function(comment) {
-        var comment_str = $(comment).text();
-        if (comment_str.length > 50) {
-            var special_characters = ['★', '☞', '✓', '♫', '☂', '◁'];
-            for (var i = 0; i < special_characters.length; i++) {
-                if ($(comment).text().indexOf(special_characters[i]) >= 0) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-];
+/* Check if image exists */
+var hasImage = function(elem) {
+  return $($(elem).find(".UFICommentContent")[0]).children("div").length == 2;
+}
 
-var checkComment = function(comment) {
-    for (var i=0; i<rules.length; i++) {
-        if (rules[i](comment)) {
-            return true;
-        }
+/* Set of filtering rules */
+var rules = [
+  function(comment) { // Recognize images
+    if (hasImage(comment)) {
+      return true;
     }
     return false;
+  },
+  function(comment) { // Recognize texts with special characters
+    var comment_str = $(comment).text();
+    if (comment_str.length > 100) {
+      var special_characters = ['★', '※', '☞', '✓', '♫', '☂', '◁', '▶'];
+      for (var i = 0; i < special_characters.length; i++) {
+        if (comment_str.indexOf(special_characters[i]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  /* TO ADD MORE */
+];
+
+/* Apply rules to each comment */
+var checkComment = function(comment) {
+  for (var i = 0; i < rules.length; i++) {
+    if (rules[i](comment)) {
+      return true;
+    }
+  }
+  return false;
 };
 
+/* Executed upon automatic comment load */
 var onCommentLoad = function(event) {
-    if ($(event.target).find(".UFIComment").length > 0) {
-        console.log(event); 
+  $(event.target).find(".UFIComment").each(function(index) {
+    if (!$(this).hasClass("co-checked") && checkComment(this)) {
+      var filteredArea = $(this).find(".UFICommentBody");
+      if (hasImage(this)) {
+        filteredArea = filteredArea.add($($(this).find(".UFICommentContent")[0]).children("div"));
+      }
+      filteredArea.css("display", "none");
+      var btn = $("<a></a>");
+      var hide = $("<span>Hide</span>");
+      var show = $("<span>Show</span>");
+      btn.append(hide);
+      hide.hide();
+      btn.append(show);
+      btn.click(function() {
+        filteredArea.toggle();
+        hide.toggle();
+        show.toggle();
+      });
+      btn.css({
+        "display"      : "inline-block",
+        "padding-left" : "3px",
+        "color"        : "#ee6e73"
+      });
+      $($($(this).find(".UFICommentContent")[0]).children("span")[0]).after(btn);
     }
-    $(event.target).find(".UFIComment").each(function(index) {
-        console.log("I'm in!");
-        if (checkComment(this)) {
-            if (!$(this).hasClass("removed")) {
-                // $(this).children().css("display", "none");
-                $(this).children().css("background-color", "red");
-                var _this = $(this).children();
-                $(this).addClass("removed");
-                var btn = $("<a></a>");
-                var hide = $("<p>Hide</p>");
-                var show = $("<p>Show</p>");
-                btn.append(hide);
-                btn.append(show);
-                btn.click(function() {
-                    _this.toggle();
-                    hide.toggle();
-                    show.toggle();
-                });
-                $(this).append(btn);
-                hide.toggle();
-            }
-        }
-    });
+    $(this).addClass("co-checked");
+  });
 };
+
+/* Executed upon more comments requested */
+var onViewMoreComments = function(event) {
+  $(event.target).parents(".UFIList").bind("DOMSubtreeModified", onCommentLoad);
+}
 
 $("body").bind("DOMNodeInserted", onCommentLoad);
+$("body").on("click", ".UFIPagerLink", onViewMoreComments);
