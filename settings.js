9e87a00c0e57e4e6ea2927fa718f234235396f50
Min Woo Kim <MinWooKim@Mins-MacBook-Air.local>
2016-06-24 16:29:12 +0900

    Create list

--- settings.js
+++ settings.js
@@ -1,3 +1,4 @@
+/*
 $(function() {
     $("#dddd").click(function() {
         chrome.storage.local.set({'value': 'ccccc'}, function() {
@@ -16,6 +17,57 @@ $(function() {
             $(_this).append("<div>" + object['another_value'] + "</div>");
         });
     });
-
 });
+*/
+var id = 0;
+function Task(task) {
+	this.id = id++;
+	this.task = task;
+	this.createLi = function() {
+    //<li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
+		var li = $( "<li class='collection-item' id='" + this.id + "'>" +
+                  "<div>" + this.task +
 
+                    /*
+                    "<input type='checkbox' class='filled-in' id='filled-in-box' checked='checked' />" +
+                    "<label for='filled-in-box'>" +
+                    this.task +
+                    "</label>" +
+                    */
+                    "<a href='javascript:;' class='remove-btn secondary-content'>" +
+                      "<i class='material-icons'>clear</i>" +
+                    "</a>" +
+                  "</div>" +
+                "</li>");
+		return li;
+	};
+}
+
+var addNewTask = function(task) {
+	var newTask = new Task(task);
+	$( "#task-list" ).append(newTask.createLi());
+
+	var li = $( "#" + newTask.id );
+
+	li.find(".remove-btn").on("click", function() {
+		li.remove();
+	});
+  /*
+	li.find(".a-task").on("click", function() {
+		$( this ).toggleClass("is-done");
+	});
+  */
+};
+
+$( document ).ready(function() {
+	$( "#input-task" ).on("keypress", function(e) {
+		var key = e.which || e.keyCode;
+		if (key === 13) {
+			var task = $( "#input-task" ).val();
+    	if (task !== "") {
+    		$( "#input-task" ).val("");
+      	addNewTask(task);
+    	}
+    }
+	});
+});
